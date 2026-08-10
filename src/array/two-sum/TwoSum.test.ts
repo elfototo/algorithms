e4202b0d1670ca77a1392639966ca98db44899db
elfototo <e89617881302@gmail.com>
2026-08-10 18:13:55 +0700

    test(two-sum): add test cases

---
+++ src/array/two-sum/TwoSum.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "vitest";
+import twoSum from "./TwoSum.js";
+
+test("finds a pair that adds up to target", () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([1, 0]);
+});
+
+test("find a pair of non-edjacment numbers", () => {
+  expect(twoSum([2, 13, 4, 5], 9)).toEqual([3, 2]);
+});
+
+test("returns an empty array if there is not pair", () => {
+    expect(twoSum([2, 4, 6, 1], 9)).toEqual([]);
+})
+
+test("works with negative numbers", () => {
+    expect(twoSum([-1, -2, 8, 3], -3)).toEqual([1, 0]);
+})
